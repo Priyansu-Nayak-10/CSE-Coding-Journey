858a428d13d8d10eea0dee704cc7a7767a43dc04
Priyansu-Nayak-10 <nayakpriyansu385@gmail.com>
2025-04-03 21:52:18 +0530

    Update

---
+++ SEM-1 (ICP)/Assignment2/Question2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Question2 {
+	public static void main(String[] args) {
+		double km, meters, inches, feet, centimeter;
+		Scanner sc = new Scanner(System.in); 
+		System.out.print("Enter a distance in km: ");
+		km=sc.nextDouble();
+		meters=km*1000;
+		feet=3280.8399*km;
+		inches=39370.0787*km;
+		centimeter=100000*km;
+		System.out.println(km+" kilometers is "+meters+" meters");
+		System.out.println(km+" kilometers is "+feet+" feet");
+		System.out.println(km+" kilometers is "+inches+" inches");
+		System.out.println(km+" kilometers is "+centimeter+" centimeter");
+	}
+
+}
---
+++ SEM-1 (ICP)/Assignment2/Question3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Question3 {
+
+	public static void main(String[] args) {
+		double salary, DA, HRA, gross;
+		Scanner sc = new Scanner(System.in); 
+		System.out.print("Enter basic salary: ");
+		salary=sc.nextDouble();
+		DA=0.4*salary;
+		HRA=0.2*salary;
+		gross=salary+DA+HRA;
+		System.out.println("DA is "+DA);
+		System.out.println("HRA is "+HRA);
+		System.out.println("Gross Salary is "+gross);
+	}
+
+}
